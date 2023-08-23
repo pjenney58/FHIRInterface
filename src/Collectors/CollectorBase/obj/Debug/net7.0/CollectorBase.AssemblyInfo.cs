0aa7b2e39c500f534b5b766e30edd8bfe0e22222
Pete Jenney <pete.jenney@gmail.com>
2023-08-23 11:50:22 -0400

    Sync

--- src/Collectors/CollectorBase/obj/Debug/net7.0/CollectorBase.AssemblyInfo.cs
+++ src/Collectors/CollectorBase/obj/Debug/net7.0/CollectorBase.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("0acb847a-861e-4cfc-b8ad-ec127bb596a0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollectorBase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
