9c93bb45072b799bd3fee074e320415e17d2c282
johnbarhorst <johnbarhorst.dev@gmail.com>
2023-09-04 14:30:14 -0500

    view details button and types

--- src/ui-sys-admin/src/pages/admin/customers/index.tsx
+++ src/ui-sys-admin/src/pages/admin/customers/index.tsx
@@ -8,21 +8,29 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { ColDef } from 'ag-grid-community';
+
+interface ColDefTenant extends Tenant {
+  viewDetails?: string;
+}
+
+type CustomColDef = ColDef<ColDefTenant>;
 
 
-const initColumnDefs = [
+const initColumnDefs: CustomColDef[] = [
   { field: 'name', headerName: 'Name', width: 200, filter: true },
   { field: 'phoneNumber', headerName: 'Phone Number', width: 200, filter: true },
   { field: 'billingInfo.paymentStatus', headerName: 'Payment Status', width: 200 },
+  { field: 'viewDetails', headerName: 'View Details', width: 200, cellRenderer: ViewDetailsButton },
 ]
 
 export default function Customers() {
-  const [columnDefs, setColumnDefs] = useState<any[]>(initColumnDefs);
-  const [rowData, setRowData] = useState<Tenant[]>([]);
+  const [columnDefs, setColumnDefs] = useState<CustomColDef[]>(initColumnDefs);
+  const [rowData, setRowData] = useState<ColDefTenant[]>([]);
   const router = useRouter();
 
   const getRowId = useMemo(() => {
-    return (params: { data: Tenant }) => params.data.id;
+    return (params: { data: ColDefTenant }) => params.data.id;
   }, []);
 
 
@@ -37,23 +45,21 @@ export default function Customers() {
     <div className={style.container}>
       <h1>Customers</h1>
       <div className="ag-theme-alpine" style={{ width: '100%', height: 500 }} >
-        <AgGridReact<Tenant>
+        <AgGridReact<ColDefTenant>
           columnDefs={columnDefs}
           rowData={rowData}
           getRowId={getRowId}
           animateRows={true}
           pagination={true}
-          onRowClicked={(e) => router.push(`/admin/customers/${e.data.id as string}`)}
-
         />
       </div>
     </div>
   )
 }
 
-function ViewDetailsButton({ id }: { id: string }) {
+function ViewDetailsButton(params: { data: ColDefTenant }) {
   return (
-    <Link href={`/admin/customers/${id}`}>
+    <Link href={`/admin/customers/${params.data.id}`}>
       <button className='button'>View Details</button>
     </Link>
   );
