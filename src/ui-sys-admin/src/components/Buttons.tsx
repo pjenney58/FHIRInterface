8acf87a16d1d41def252c9b7bb7f2e492346c9b5
johnbarhorst <johnbarhorst.dev@gmail.com>
2023-09-06 20:45:57 -0500

    some color

--- src/ui-sys-admin/src/components/Buttons.tsx
+++ src/ui-sys-admin/src/components/Buttons.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { ColDefCustomer } from "pages/admin/customers";
 import { ColDefUser } from "pages/admin/users";
+import style from 'styles/Buttons.module.css';
 
 export interface ControlButtonsProps {
     // Better way to do this with generics? Using generics makes id possibly undefined
@@ -13,14 +14,14 @@ export function ControlButtons(params: ControlButtonsProps) {
     const router = useRouter();
     const baseURL = router.pathname;
     return (
-        <div>
+        <div className={style.controlsWrapper}>
             <Link href={`${baseURL}/${params.data.id}`}>
                 <button className='button' >View</button>
             </Link>
             <Link href={`${baseURL}/${params.data.id}/edit`}>
                 <button className='button' >Edit</button>
             </Link>
-            <button className='button' onClick={() => params.handleDelete()}>Delete</button>
+            <button className={`${style.delete} button`} onClick={() => params.handleDelete()}>Delete</button>
         </div>
     );
 }
